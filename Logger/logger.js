d87e7edc06fcbae311b52d1dbf219117ca8a105b
Simon <yash1441@yahoo.com>
2023-01-17 12:13:28 +0530

    Update logger.js
    Added timestamp, color

--- Logger/logger.js
+++ Logger/logger.js
@@ -1,8 +1,18 @@
-const winston = require("winston");
+const { format, createLogger, transports } = require("winston");
+const { timestamp, combine, printf, colorize } = format;
 
-const logger = winston.createLogger({
-	format: winston.format.simple(),
-	transports: [new winston.transports.Console()],
+const logFormat = printf(({ level, message, timestamp }) => {
+	return `${timestamp} [${level.toUpperCase()}]: ${message}`;
+});
+
+const logger = createLogger({
+	level: "debug",
+	format: combine(
+		colorize(),
+		timestamp({ format: "DD-MM-YYYY HH:mm:ss" }),
+		logFormat
+	),
+	transports: [new transports.Console()],
 });
 
 module.exports = logger;
