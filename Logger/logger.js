1b283c2ef95191af4b2a6353bd9fbd466dda0383
Simon <yash1441@yahoo.com>
2023-01-17 13:04:05 +0530

    Update logger.js
    Created debugLogger

--- Logger/logger.js
+++ Logger/logger.js
@@ -1,19 +1,33 @@
 const { createLogger, format, transports } = require("winston");
-const { combine, timestamp, label, printf, colorize, errors } = format;
+const { combine, timestamp, label, printf, colorize, errors, json } = format;
 
 const logFormat = printf(({ level, message, timestamp, stack }) => {
 	return `${timestamp} ${level}: ${stack || message}`;
 });
 
-const logger = createLogger({
+const debugLogger = createLogger({
 	level: "debug",
 	format: combine(
 		colorize(),
-		timestamp({ format: 'DD-MM-YY HH:mm:ss' }),
+		timestamp({ format: "DD-MM-YY HH:mm:ss" }),
 		errors({ stack: true }),
 		logFormat
 	),
 	transports: [new transports.Console()],
 });
 
-module.exports = logger;
+const logger = createLogger({
+	level: "info",
+	format: combine(
+		timestamp({ format: "DD-MM-YY HH:mm:ss" }),
+		errors({ stack: true }),
+		json()
+	),
+	transports: [
+		new winston.transports.File({ filename: "error.log", level: "error" }),
+		new winston.transports.File({ filename: "warn.log", level: "warn" }),
+		new winston.transports.File({ filename: "info.log", level: "info" }),
+	],
+});
+
+module.exports = { logger, debugLogger };
