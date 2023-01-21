31a4ea3ef8aec5f13dc7eefbcd298dcdd1e738d0
Simon <yash1441@yahoo.com>
2023-01-21 14:33:53 +0530

    Update index.js
    Updated twitch notification

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const {
 const fs = require("fs");
 const path = require("path");
 const TwitchApi = require("node-twitch").default;
+const moment = require("moment");
 require("dotenv").config();
 const logger = require("./Logger/logger.js");
 
@@ -58,7 +59,7 @@ for (const file of commandFiles) {
 //// BOT EVENTS ////
 ////////////////////
 
-client.on("ready", () => {
+client.on("ready", async () => {
 	logger.info(`Discord bot went online. Username: ${client.user.tag}`);
 	client.user.setPresence({
 		activities: [
@@ -90,15 +91,7 @@ client.on("ready", () => {
 	});
 
 	setInterval(() => {
-		twitch.getStreams({ channel: "ygamingplay" }).then((response) => {
-			if (response.data.length > 0 && !isLive) {
-				isLive = true;
-				const channel = client.channels.cache.get("770859213273694208");
-				channel.send("ESL CSGO is live! Go check out their stream!");
-			} else if (response.data.length == 0) {
-				isLive = false;
-			}
-		});
+		checkLive("ygamingplay");
 	}, 10000);
 });
 
@@ -120,3 +113,108 @@ client.on("interactionCreate", async (interaction) => {
 });
 
 client.login(process.env.DISCORD_TOKEN);
+
+async function checkLive(channel) {
+	const stream = await getChannel(channel);
+	if (stream && !isLive) {
+		// Stream went LIVE //
+		isLive = true;
+
+		const user = await getUser(stream.user_login);
+		if (!user) return console.log("User not found - " + stream.user_login);
+
+		const game = await getGame(parseInt(stream.game_id));
+		if (!game) return console.log("Game not found - " + stream.game_name);
+
+		await sendLiveEmbed(stream, user, game, "770859213273694208");
+	} else if (!stream && isLive) {
+		// Stream went OFFLINE //
+		isLive = false;
+		return;
+	} else if (stream && isLive) {
+		// Stream LIVE update //
+		return;
+	} else {
+		// Stream OFFLINE update //
+		return;
+	}
+}
+
+async function getChannel(channel) {
+	const stream = await twitch.getStreams({ channel: channel });
+	if (stream.data.length > 0) {
+		const stream_data = {
+			user_id: stream.data[0].user_id,
+			user_login: stream.data[0].user_login,
+			user_name: stream.data[0].user_name,
+			game_id: stream.data[0].game_id,
+			game_name: stream.data[0].game_name,
+			title: stream.data[0].title,
+			viewer_count: stream.data[0].viewer_count,
+			started_at: stream.data[0].started_at,
+			thumbnail_url: stream.data[0].thumbnail_url
+				.replace("{width}", "1280")
+				.replace("{height}", "720"),
+			tags: stream.data[0].tags,
+		};
+		return stream_data;
+	} else return false;
+}
+
+async function getUser(user_login) {
+	const user = await twitch.getUsers(user_login);
+	if (user.data.length > 0) {
+		const user_data = {
+			id: user.data[0].id,
+			broadcaster_type: user.data[0].broadcaster_type,
+			description: user.data[0].description,
+			profile_image_url: user.data[0].profile_image_url,
+			offline_image_url: user.data[0].offline_image_url,
+			view_count: user.data[0].view_count,
+			created_at: user.data[0].created_at,
+		};
+		return user_data;
+	} else return false;
+}
+
+async function getGame(game_id) {
+	const game = await twitch.getGames(game_id);
+	if (game.data.length > 0) {
+		const game_data = {
+			box_art_url: game.data[0].box_art_url
+				.replace("{width}", "300")
+				.replace("{height}", "300"),
+			igdb_id: game.data[0].igdb_id,
+		};
+		return game_data;
+	} else return false;
+}
+
+async function sendLiveEmbed(stream, user, game, channelId) {
+	const channel = client.channels.cache.get(channelId);
+	const embed = new EmbedBuilder()
+		.setAuthor({
+			name: stream.user_name,
+			iconURL: user.profile_image_url,
+			url: "https://twitch.tv/" + stream.user_name,
+		})
+		.setThumbnail(game.box_art_url)
+		.setTitle(stream.title)
+		.setURL("https://twitch.tv/" + stream.user_name)
+		.addFields(
+			{ name: "Game", value: stream.game_name, inline: true },
+			{ name: "Viewers", value: stream.viewer_count, inline: true },
+			{ name: "Tags", value: "`" + stream.tags.join("``") + "`", inline: true }
+		)
+		.setImage(
+			stream.thumbnail_url.replace("{width}", "1280").replace("{height}", "720")
+		)
+		.setFooter({
+			text: "Started <t:" + moment(stream.started_at).valueOf() / 1000 + ":R>",
+		});
+	channel.send({
+		content: `${stream.user_name} is **Live**!`,
+		embeds: [embed],
+		//component: [row],
+	});
+}
