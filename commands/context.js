8f244b0ff06fc58367389b2480729f946a801d5f
Simon <yash1441@yahoo.com>
2023-02-06 23:17:06 +0530

    Create context.js
    Added context menu test

---
+++ commands/context.js
@@ -0,0 +1,18 @@
+const {
+	ContextMenuCommandBuilder,
+	ApplicationCommandType,
+	EmbedBuilder,
+} = require("discord.js");
+
+module.exports = {
+	data: new ContextMenuCommandBuilder()
+		.setName("test")
+		.setType(ApplicationCommandType.User),
+	async execute(interaction) {
+		const embed = new EmbedBuilder()
+			.setTitle("Test")
+			.setDescription("This is a test")
+			.setColor("RANDOM");
+		await interaction.reply({ ephemeral: true, embeds: [embed] });
+	},
+};
