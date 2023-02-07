1d21c54548b5e30314398acb54474557f2607672
Simon <yash1441@yahoo.com>
2023-02-07 17:23:14 +0530

    Updated editembed

--- commands/editembed.js
+++ commands/editembed.js
@@ -2,6 +2,10 @@ const {
 	ContextMenuCommandBuilder,
 	ApplicationCommandType,
 	EmbedBuilder,
+	ModalBuilder,
+	ActionRowBuilder,
+	TextInputBuilder,
+	TextInputStyle,
 } = require("discord.js");
 
 module.exports = {
@@ -12,6 +16,55 @@ module.exports = {
 	async execute(interaction) {
 		const message = interaction.targetMessage;
 		const embed = message.embeds[0];
+
+		const fields = {
+			title: new TextInputBuilder()
+				.setCustomId("title")
+				.setPlaceholder(embed.title)
+				.setStyle(TextInputStyle.Short)
+				.setLabel("Title")
+				.setMinLength(1)
+				.setMaxLength(256),
+			description: new TextInputBuilder()
+				.setCustomId("description")
+				.setPlaceholder(embed.description)
+				.setStyle(TextInputStyle.Long)
+				.setLabel("Description")
+				.setMinLength(1)
+				.setMaxLength(4096),
+		};
+
+		const modal = new ModalBuilder()
+			.setCustomId("editEmbed")
+			.setTitle("Edit Embed")
+			.setComponents(
+				new ActionRowBuilder().setComponents(fields.title),
+				new ActionRowBuilder().setComponents(fields.description)
+			);
+
+		await interaction.showModal(modal);
+
+		const submit = await interaction
+			.awaitModalSubmit({
+				time: 60000,
+			})
+			.catch((error) => {
+				console.log(error);
+				return await interaction.reply({ content: "Timed out.", ephemeral: true });
+			});
+
+		if (submit) {
+			let newEmbed = new EmbedBuilder()
+				.setTitle(submit.fields.getTextInputValue(fields.title.customId))
+				.setDescription(
+					submit.fields.getTextInputValue(fields.description.customId)
+				);
+
+			await message.edit({ embeds: [newEmbed] });
+
+			await submit.reply({ content: "Embed edited.", ephemeral: true });
+		}
+
 		console.log(embed);
 	},
 };
