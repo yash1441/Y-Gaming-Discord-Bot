4de6f7cd30c4c5c0869b14fea6720ce230d19bf7
Simon <yash1441@yahoo.com>
2023-02-20 15:41:10 +0530

    Formatting changes

--- index.js
+++ index.js
@@ -136,9 +136,15 @@ client.on("interactionCreate", async (interaction) => {
 
 			for (const skin of skins) {
 				const skinEmbed = new EmbedBuilder()
+					.setAuthor()
+					.setColor("#36393F")
 					.setTitle(skin.name)
 					.setThumbnail(skin.icon)
-					.setDescription(skin.discountCosts + ` ||${skin.discountPercent}%||`);
+					.setDescription(
+						"<:VP:1077169497582080104> " +
+							skin.discountCosts +
+							`    ||*-${skin.discountPercent}%*||`
+					);
 				embeds.push(skinEmbed);
 			}
 
