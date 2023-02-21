d404025c610b7a1e1dfa44de1140bb92e24d2cd2
Simon <yash1441@yahoo.com>
2023-02-21 15:34:51 +0530

    Removed test

--- commands/editembed.js
+++ commands/editembed.js
@@ -51,10 +51,7 @@ module.exports = {
 		if (submit) {
 			let newEmbed = new EmbedBuilder()
 				.setTitle(submit.fields.getTextInputValue("title"))
-				.setDescription(
-					submit.fields.getTextInputValue("description") +
-						" <:RAUL_INDENT:700506263167172738>"
-				)
+				.setDescription(submit.fields.getTextInputValue("description"))
 				.setColor(embed.color);
 
 			if (message.id == submit.customId.substring(9)) {
