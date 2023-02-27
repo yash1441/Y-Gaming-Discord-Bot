20879e32ac05c4244c9d169c1e1ad8b48c39794d
Simon <yash1441@yahoo.com>
2023-02-28 00:39:59 +0530

    Fixed debug

--- commands/lastmatch.js
+++ commands/lastmatch.js
@@ -275,13 +275,13 @@ async function createScoreboard(interaction, players, map, date, file) {
 
 	for (i = 0, id = 0; i < 500; i += 100, id += 1) {
 		// Agent
-		logger.debug("Agent: " + players.red[id].character);
+		if (!players.red[id] == undefined) logger.debug(players.red[id]);
 		mapImage.composite(
 			eval(players.red[id].character.replace("/", "_")),
 			agentX,
 			agentY + i
 		); //Red
-		logger.debug("Agent: " + players.blue[id].character);
+		if (!players.blue[id] == undefined) logger.debug(players.blue[id]);
 		mapImage.composite(
 			eval(players.blue[id].character.replace("/", "_")),
 			agentX + agentDiff,
