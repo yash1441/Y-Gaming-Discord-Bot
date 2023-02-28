f745630676c621fb0e75cf7c34876bb81379807e
Simon <yash1441@yahoo.com>
2023-02-28 10:49:05 +0530

    Added store command

---
+++ commands/store.js
@@ -0,0 +1,49 @@
+const {
+	SlashCommandBuilder,
+	EmbedBuilder,
+	ModalBuilder,
+	TextInputBuilder,
+	TextInputStyle,
+	ActionRowBuilder,
+} = require("discord.js");
+const HenrikDevValorantAPI = require("unofficial-valorant-api");
+const vapi = new HenrikDevValorantAPI();
+const axios = require("axios");
+const logger = require("../Logger/logger.js");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("store")
+		.setDescription("Check store in Valorant"),
+	async execute(interaction, client) {
+		await interaction.deferReply();
+		let store;
+		await vapi
+			.getFeaturedItems({
+				version: "v2",
+			})
+			.then((response) => {
+				store = response;
+			})
+			.catch((error) => {
+				logger.error(error);
+			});
+
+		const embeds = [];
+
+		for (const bundle of store.data) {
+			const bundleUUID = bundle.uuid;
+			const bundleData = await axios.get(
+				`https://valorant-api.com/v1/bundles/${bundleUUID}`
+			);
+			const embed = new EmbedBuilder()
+				.setTitle(bundleData.data.data.displayName)
+				.setColor("FA4454")
+				.setImage(bundleData.data.data.displayIcon);
+
+			embeds.push(embed);
+		}
+
+		await interaction.editReply({ embeds: embeds });
+	},
+};
