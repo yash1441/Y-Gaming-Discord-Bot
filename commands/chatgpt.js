8e5c4752b921e8fcd83bb067b5332850fb9d24e4
Simon <yash1441@yahoo.com>
2023-04-04 14:03:56 +0530

    Fixed message content

--- commands/chatgpt.js
+++ commands/chatgpt.js
@@ -22,10 +22,10 @@ module.exports = {
 
 		const gptResponse = await openai.createCompletion({
 			model: "davinci",
-			prompt: `The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\n${message.author.username}: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\n${message.author.username}: ${message.content}\nAI:`,
+			prompt: `The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\n${interaction.user.username}: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\n${interaction.user.username}: ${interaction.message.content}\nAI:`,
 			temperature: 0.9,
 			max_tokens: 100,
-			stop: ["\n", `${message.author.username}:`, "AI:"],
+			stop: ["\n", `${interaction.user.username}:`, "AI:"],
 		});
 
 		await message.editReply(gptResponse.data.choices[0].text);
