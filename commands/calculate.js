3a1e1412c08f876513c539ef26a763daac66d0ad
Simon <yash1441@yahoo.com>
2023-06-04 19:19:10 +0530

    Added calculate

---
+++ commands/calculate.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder } = require("discord.js");
+const logger = require("../Logger/logger.js");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("calculate")
+		.setDescription("Calculate a given math expression.")
+		.addStringOption((option) =>
+			option
+				.setName("math")
+				.setDescription("Math expression to calculate")
+				.setRequired(true)
+		),
+	async execute(interaction, client) {
+		let math = interaction.options.getString("math");
+        const compute = (math = '') => {
+            let total = 0;
+            math = math.match(/[+\−]*(\.\d+|\d+(\.\d+)?)/g) || [];
+            while (math.length) {
+                total += parseFloat(math.shift());
+            };
+            return total;
+        };
+
+		await interaction.reply({ content: compute(math), ephemeral: true });
+	},
+};
