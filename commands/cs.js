8fe35e22b8f442f764ff264fd5edbd01dee7c504
Simon <yash1441@yahoo.com>
2023-07-08 13:50:23 +0530

    Fixed getString

--- commands/cs.js
+++ commands/cs.js
@@ -13,11 +13,11 @@ module.exports = {
 				.setName("rank")
 				.setDescription("Get the CS:GO rank of a player.")
                 .addStringOption((option) =>
-                option
-                    .setName("steam-id")
-                    .setDescription("Enter the Steam ID of the player. Example: STEAM_1:1:123456789")
-                    .setRequired(true)
-                )
+                    option
+                        .setName("steam-id")
+                        .setDescription("Enter the Steam ID of the player. Example: STEAM_1:1:123456789")
+                        .setRequired(true)
+                    )
 		),
 	async execute(interaction) {
 		const subCommand = interaction.options.getSubcommand();
@@ -25,7 +25,7 @@ module.exports = {
         if (subCommand === "rank") {
             await interaction.reply({ content: "Checking if it is a valid Steam ID...", ephemeral: true });
             
-            const steamId = interaction.options.getChannel("steam-id");
+            const steamId = interaction.options.getString("steam-id");
 
             if (!steamId.startsWith("STEAM_")) {
                 return await interaction.editReply({ content: `\`${steamId}\` is an invalid Steam ID.` });
