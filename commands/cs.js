14cf2a7fd155a86337f35d25b9faecf2cdc9f611
Simon <yash1441@yahoo.com>
2023-07-08 13:47:56 +0530

    Added cs command

---
+++ commands/cs.js
@@ -0,0 +1,37 @@
+const { SlashCommandBuilder, ChannelType, EmbedBuilder, PermissionFlagsBits, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+const fs = require("fs");
+const path = require("path");
+const logger = require("../Logger/logger.js");
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("cs")
+		.setDescription("Commands related to Counter-Strike.")
+		.setDMPermission(false)
+		.addSubcommand((subcommand) =>
+			subcommand
+				.setName("rank")
+				.setDescription("Get the CS:GO rank of a player.")
+                .addStringOption((option) =>
+                option
+                    .setName("steam-id")
+                    .setDescription("Enter the Steam ID of the player. Example: STEAM_1:1:123456789")
+                    .setRequired(true)
+                )
+		),
+	async execute(interaction) {
+		const subCommand = interaction.options.getSubcommand();
+
+        if (subCommand === "rank") {
+            await interaction.reply({ content: "Checking if it is a valid Steam ID...", ephemeral: true });
+            
+            const steamId = interaction.options.getChannel("steam-id");
+
+            if (!steamId.startsWith("STEAM_")) {
+                return await interaction.editReply({ content: `\`${steamId}\` is an invalid Steam ID.` });
+            }
+
+            await interaction.editReply({ content: `\`${steamId}\` is a valid Steam ID.` });
+        }
+	},
+};
