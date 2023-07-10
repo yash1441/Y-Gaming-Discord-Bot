9a8b3a46a11cece3b3d2e022e0cd007d236bf378
Simon <yash1441@yahoo.com>
2023-07-10 15:32:38 +0530

    Defined Sequelize

--- commands/valo.js
+++ commands/valo.js
@@ -15,6 +15,7 @@ const vapi = new HenrikDevValorantAPI();
 const axios = require("axios");
 const logger = require("../Logger/logger.js");
 
+const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
 	host: process.env.DB_IP,
 	dialect: 'mysql',
