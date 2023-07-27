211aa62117717dae285d9ef57c102a8b76424088
Simon <yash1441@yahoo.com>
2023-07-27 18:11:41 +0530

    Added crosshair decode

--- commands/cs.js
+++ commands/cs.js
@@ -3,6 +3,7 @@ const logger = require("../Logger/logger.js");
 const SteamID = require("steamid");
 const cheerio = require("cheerio");
 const cloudscraper = require("cloudscraper");
+const { decodeCrosshairShareCode, Crosshair } = require("csgo-sharecode");
 
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
@@ -41,11 +42,22 @@ module.exports = {
 		.addSubcommand((subcommand) =>
 			subcommand
 				.setName("rank")
-				.setDescription("Get the CS:GO rank of a player.")
+				.setDescription("Get the rank of a player.")
                 .addStringOption((option) =>
                     option
                         .setName("steam-id")
                         .setDescription("Enter the Steam ID of the player. Example: STEAM_1:1:123456789")
+                        .setRequired(true)
+                    )
+		)
+        .addSubcommand((subcommand) =>
+			subcommand
+				.setName("crosshair")
+				.setDescription("Get the crosshair settings.")
+                .addStringOption((option) =>
+                    option
+                        .setName("crosshair-code")
+                        .setDescription("Enter the crosshair code.")
                         .setRequired(true)
                     )
 		),
@@ -81,6 +93,13 @@ module.exports = {
                 .setColor("Random");
 
             await interaction.editReply({ content: "", embeds: [embed] });
+        } else if (subCommand === "crosshair") {
+            await interaction.reply({ content: "Checking if it is a valid crosshair code..." });
+            
+            const shareCode = interaction.options.getString("crosshair-code");
+
+            const crosshair = decodeCrosshairShareCode(shareCode);
+            console.log(crosshair);
         }
 	},
 };
