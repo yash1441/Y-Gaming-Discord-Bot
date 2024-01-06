3b89f0c02481996f89ebe4af7048bd8c2e7e3500
Simon <yash1441@yahoo.com>
2024-01-07 01:28:20 +0530

    Update valo.js

--- commands/valo.js
+++ commands/valo.js
@@ -616,7 +616,7 @@ module.exports = {
                 .setOverlay('#23272A')
                 .setStatus('none')
                 .setTextStyles({ xp: "RR:" })
-                .setStyles({ statistics: { xp: { text: { style: { 'font-weight': 900 } }, value: { style: { color: ratingColor } } } } })
+                .setStyles({ statistics: { xp: { text: { style: { 'font-size': '1.5em' } }, value: { style: { color: ratingColor } } } } })
                 .setGraphemeProvider(BuiltInGraphemeProvider.FluentEmojiFlat);
 
             if (playerCardWide != "None") {
