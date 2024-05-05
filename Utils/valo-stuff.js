71eee6c84a93c01e70436f881832551b07785e58
Simon <yash1441@yahoo.com>
2024-05-05 20:19:31 +0530

    Added cache-control and keep-alive

--- Utils/valo-stuff.js
+++ Utils/valo-stuff.js
@@ -78,14 +78,16 @@ async function authCookies(build) {
         url: 'https://auth.riotgames.com/api/v1/authorization',
         headers: {
             'Content-Type': 'application/json',
-            'User-Agent': build.userAgent
+            'User-Agent': build.userAgent,
+            'Cache-Control': 'no-cache',
+            'Accept': 'application/json',
+            'Keep-Alive': true
         },
         data: {
             client_id: 'play-valorant-web-prod',
             nonce: '1',
             redirect_uri: 'https://playvalorant.com/opt_in',
             response_type: 'token id_token',
-            // response_mode: 'query',
             scope: 'account openid'
         },
         httpsAgent: agent
