57e1bc4c046be7258c082999e8d4ba7dac975cc6
Simon <yash1441@yahoo.com>
2024-05-05 20:10:57 +0530

    Added more ciphers and disabled cache

--- Utils/valo-stuff.js
+++ Utils/valo-stuff.js
@@ -3,16 +3,28 @@ const { Agent } = require("https");
 const logger = require("../Logger/logger.js");
 
 const ciphers = [
-    'TLS_CHACHA20_POLY1305_SHA256',
     'TLS_AES_128_GCM_SHA256',
     'TLS_AES_256_GCM_SHA384',
-    'TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256'
+    'TLS_CHACHA20_POLY1305_SHA256',
+    'ECDHE-ECDSA-AES128-GCM-SHA256',
+    'ECDHE-RSA-AES128-GCM-SHA256',
+    'ECDHE-ECDSA-AES256-GCM-SHA384',
+    'ECDHE-RSA-AES256-GCM-SHA384',
+    'ECDHE-ECDSA-CHACHA20-POLY1305',
+    'ECDHE-RSA-CHACHA20-POLY1305',
+    'ECDHE-RSA-AES128-SHA',
+    'ECDHE-RSA-AES256-SHA',
+    'AES128-GCM-SHA256',
+    'AES256-GCM-SHA384',
+    'AES128-SHA',
+    'AES256-SHA'
 ];
 
 const agent = new Agent({
     ciphers: ciphers.join(':'),
     honorCipherOrder: true,
-    minVersion: 'TLSv1.3'
+    minVersion: 'TLSv1.3',
+    maxCachedSessions: 0
 });
 
 /// VALORANT VERSION ///
